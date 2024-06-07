fe4de850fa57129343dd9c97e4bdc44072b24a27
Astois <astois@astois.Astois!>
2024-06-07 23:43:28 +0100

    Bubble-sort works now, i had to change print function to look for regular variables rather than pointers, changed the while loop to always be sorted untill a swap occurs so that if no swap occurs it means the list is sorted and the loop ends

--- BSort.c
+++ BSort.c
@@ -1,15 +1,21 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// swap function that takes item a and stores it in a temp variable
+//  it makes item a item b, and makes the value of b the value of temp
 void swap(int *a, int *b) {
   int temp = *a;
   *a = *b;
   *b = temp;
 }
-void printArr(int a[], int *b) {
-  for (int i = 0; i < *b; i++) {
-    printf("%d", a[i]);
+
+// takes the list and the amount of items in the list
+// it then iterates through the numlist and prints each element
+void printArr(int a[], int b) {
+  for (int i = 0; i < b; i++) {
+    printf("%d ", a[i]);
   }
+  printf("\n");
 }
 int main() {
   int numlist[] = {45, 56, 3, 34, 9, 55, 1};
@@ -17,21 +23,25 @@ int main() {
   itemnum = sizeof(numlist) / sizeof(numlist[0]);
   bool islistsorted = false;
 
+  // this while loop makes sure that the list is sorted
+  // if no swaps are made it listsorted is true
+  //  if it does make a swap then list sorted stays false and iterates through
+  //  the numlist swaping elements
+  // it checks if item 1 is bigger than 2 if it is then it swaps
+  // after one full iteration of the list, the list will be seen as sorted
+  // untill it goes back to position 0 and finds anymore swaps if no more swaps
+  // are found then islistsorted stays true meaning the loop ends and the list
+  // prints
   while (islistsorted == false) {
-    for (int i = 0; i < itemnum; i++) {
+    islistsorted = true;
+    for (int i = 0; i < itemnum - 1; i++) {
       int tick = 0;
       if (numlist[i] > numlist[i + 1]) {
         swap(&numlist[i], &numlist[i + 1]);
-
-      } else {
-        tick = tick + 1;
-      }
-      if (tick == itemnum) {
-        islistsorted = true;
+        islistsorted = false;
       }
     }
   }
-  printf("%d", numlist[0]);
-  printArr(numlist, &itemnum);
+  printArr(numlist, itemnum);
   return 0;
 }
